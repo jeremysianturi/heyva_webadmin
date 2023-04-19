f5a64189211021cbcde004c72131a43ec645c204
jeremysianturi <jery.ps@gmail.com>
2023-04-19 19:32:07 +0700

    Bug fix (version on pubspec and adding new depedencies) for containerazation with docker

--- lib/app/modules/termsofservice/views/termsofservice_view.dart
+++ lib/app/modules/termsofservice/views/termsofservice_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:get/get.dart';
 import 'package:html/parser.dart';
 import '../controllers/termsofservice_controller.dart';
