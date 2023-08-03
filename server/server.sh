83384ef3db963a139dd78a82c3f1ed792ad93bcf
jeremysianturi <jery.ps@gmail.com>
2023-08-03 17:48:44 +0700

    - revision on port number

--- server/server.sh
+++ server/server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set the port
-PORT=5050
+PORT=5000
 
 # Stop any program currently running on the set port
 echo 'preparing port' $PORT '...'
-fuser -k 5050/tcp
+fuser -k 5000/tcp
 
 # switch directories
 cd build/web/
